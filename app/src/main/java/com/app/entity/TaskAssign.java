e5ebe89704077a2ab643879c392ca4e3a4f859ea
diy-z <t19863751130@gmail.com>
2020-12-25 01:00:53 +0800

    创建了TaskAssign任务分配类

---
+++ app/src/main/java/com/app/entity/TaskAssign.java
@@ -0,0 +1,64 @@
+package com.app.entity;
+
+import java.sql.Date;
+
+public class TaskAssign {
+
+    //任务ID
+    private Integer id;
+    //任务创建者用户ID
+    private Integer creatorId;
+    //被分配者用户ID
+    private Integer assigneeId;
+    private String task;
+    private Date taskCreateTime;
+    private Date taskFinishTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Integer creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Integer getAssigneeId() {
+        return assigneeId;
+    }
+
+    public void setAssigneeId(Integer assigneeId) {
+        this.assigneeId = assigneeId;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public Date getTaskCreateTime() {
+        return taskCreateTime;
+    }
+
+    public void setTaskCreateTime(Date taskCreateTime) {
+        this.taskCreateTime = taskCreateTime;
+    }
+
+    public Date getTaskFinishTime() {
+        return taskFinishTime;
+    }
+
+    public void setTaskFinishTime(Date taskFinishTime) {
+        this.taskFinishTime = taskFinishTime;
+    }
+}

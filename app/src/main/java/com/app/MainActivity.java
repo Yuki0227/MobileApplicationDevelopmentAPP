5765e6707b4f2cf12d96bec1e283f5f3862370ca
Yu <2706289018@qq.com>
2020-12-24 09:08:53 +0800

    Initial commit

---
+++ app/src/main/java/com/app/MainActivity.java
@@ -0,0 +1,93 @@
+package com.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.app.util.CommonUtils;
+
+import org.json.JSONObject;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login;
+    private EditText et_username;
+    private EditText et_password;
+    private Handler mainHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initLayoutBind();
+        initListener();
+    }
+
+    private void initLayoutBind() {
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        btn_login = findViewById(R.id.btn_login);
+        mainHandler = new Handler();
+    }
+
+    private void initListener(){
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login(){
+        final  String username = et_username.getText().toString().trim();
+        final String password = et_password.getText().toString().trim();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    FormBody.Builder params = new FormBody.Builder();
+                    params.add("name",username);
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://8.131.250.250/user/findByName")
+                            .post(params.build())
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    JSONObject jsonObj = new JSONObject(responseData);
+                    final String a= jsonObj.getString("name");
+                    final String b =jsonObj.getString("password");
+                    mainHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(a.equals(username)&&b.equals(password)){
+                                CommonUtils.showDlgMsg(MainActivity.this,"登录成功");
+                            }else {
+                                CommonUtils.showDlgMsg(MainActivity.this,"用户名或密码错误");
+                            }
+
+                        }
+                    });
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/app/util/CommonUtils.java
@@ -0,0 +1,41 @@
+package com.app.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.widget.Toast;
+
+public class CommonUtils {
+
+    /**
+     * 显示短消息
+     * @param context 上下文
+     * @param msg 要显示的消息
+     */
+    public static void showShortMsg(Context context, String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
+
+    }
+
+    /**
+     * 显示长消息
+     * @param context 上下文
+     * @param msg 要显示的消息
+     */
+    public static void showLongMsg(Context context,String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 显示消息对话框
+     * @param context    上下文
+     * @param msg 要显示的消息
+     */
+    public static void showDlgMsg(Context context,String msg){
+        new AlertDialog.Builder(context)
+                .setTitle("显示信息")
+                .setMessage(msg)
+                .setPositiveButton("确定",null)
+                .setNegativeButton("取消",null)
+                .create().show();
+    }
+}

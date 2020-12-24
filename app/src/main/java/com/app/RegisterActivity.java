b987a703bd0ac7ffb11dad69c29a1d6d02609d91
Yu <2706289018@qq.com>
2020-12-24 22:01:11 +0800

    美化登陆界面

--- app/src/main/java/com/app/RegisterActivity.java
+++ app/src/main/java/com/app/RegisterActivity.java
@@ -1,8 +1,6 @@
 package com.app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +9,6 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.util.CommonUtils;
-import com.app.util.UserUtils;
 
 import org.json.JSONObject;
 
@@ -22,94 +19,77 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    private Button btn_login;
     private Button btn_register;
     private EditText et_name;
     private EditText et_password;
-    private Handler mainHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         initLayoutBind();
-        initListener();
-    }
 
-    private void initLayoutBind() {
-        et_name = findViewById(R.id.et_name);
-        et_password = findViewById(R.id.et_password);
-        btn_login = findViewById(R.id.btn_signin);
-        btn_register = findViewById(R.id.btn_register);
-        mainHandler = new Handler();
-    }
-
-    private void initListener() {
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                register();
             }
         });
     }
 
-    private void login() {
+
+    private void initLayoutBind() {
+        et_name = findViewById(R.id.et_name1);
+        et_password = findViewById(R.id.et_password1);
+        btn_register = findViewById(R.id.btn_register1);
+    }
+
+
+    private void register() {
         final String username = et_name.getText().toString().trim();
         final String password = et_password.getText().toString().trim();
-
         new Thread(new Runnable() {
             @Override
             public void run() {
-                boolean loginSucceed = false;
-                final String id;
-                final String name;
+                boolean registerSucceed = false;
                 try {
                     FormBody.Builder params = new FormBody.Builder();
                     params.add("name", username);
                     params.add("password", password);
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("http://8.131.250.250/user/find")
+                            .url("http://8.131.250.250/user/add")
                             .post(params.build())
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = Objects.requireNonNull(response.body()).string();
                     Log.d("responseData", responseData);
                     JSONObject jsonObj = new JSONObject(responseData);
-                    id = jsonObj.getString("id");
-                    name = jsonObj.getString("name");
-
-                    UserUtils.getInstance().setUser(Integer.parseInt(id), name, "");
-
-                    loginSucceed = true;
+                    Integer idNum = Integer.parseInt(jsonObj.getString("id"));
+                    registerSucceed = true;
                 } catch (Exception e) {
                     e.printStackTrace();
 
                 }
-                final boolean finalLoginSucceed = loginSucceed;
+
+
+                final boolean finalRegisterSucceed = registerSucceed;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(finalLoginSucceed)
-                            CommonUtils.showDlgMsg(LoginActivity.this, "登录成功");
-                        else
-                            CommonUtils.showDlgMsg(LoginActivity.this, "用户名与密码不匹配或用户不存在！");
+                        if (finalRegisterSucceed) {
+                            CommonUtils.showLongMsg(RegisterActivity.this, "注册成功");
+                            finish();
+                        } else
+                            CommonUtils.showDlgMsg(RegisterActivity.this, "注册失败！");
                     }
                 });
             }
         }).start();
-
     }
-
 
 }
